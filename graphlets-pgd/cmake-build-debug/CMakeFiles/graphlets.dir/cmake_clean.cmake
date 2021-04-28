e7c54c9d22cb0ed857e87fa0f8341e906cfb92c1
Jozkings <36714542+Jozkings@users.noreply.github.com>
2021-04-28 09:09:30 +0200

    Added files important for .exe

---
+++ graphlets-pgd/cmake-build-debug/CMakeFiles/graphlets.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/graphlets.dir/graphlets.cpp.o"
+  "graphlets.exe"
+  "graphlets.pdb"
+  "libgraphlets.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/graphlets.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ graphlets-pgd/cmake-build-debug/CMakeFiles/openmp_para_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openmp_para_test.dir/graphlets.cpp.o"
+  "libopenmp_para_test.dll.a"
+  "openmp_para_test.exe"
+  "openmp_para_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/openmp_para_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
